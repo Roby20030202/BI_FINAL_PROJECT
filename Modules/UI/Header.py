e00aebda50ed4df2785df1b028699e6df38c3840
Roby20030202 <131414983+Roby20030202@users.noreply.github.com>
2025-09-22 20:04:55 -0600

    Create Header.py

---
+++ Modules/UI/Header.py
@@ -0,0 +1,16 @@
+#BI_FINAL_PROJECT/Modules/UI/Header.py
+#Encabezado
+
+import streamlit as st
+
+def show_header(text_title: str):
+  #Layout: logo + title side by side
+  col1, col2 = st.columns([1, 6])
+
+  with col1:
+    st.image("assets/UP logo.jpg", width = 200)
+
+  with col2:
+    st.title(text_title)
+    st.caption(" Developed for: *Business Intelligence (Graduate Level)*")
+    st.acption("Instructor Edgar Avalos-Gauna (2025), Universidad Panamericana")
